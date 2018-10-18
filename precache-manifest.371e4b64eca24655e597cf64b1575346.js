f4e4d54af16c9ed4b372b3c2da58146a3f422e98
sydneycode <sstaggs12@gmail.com>
2018-10-18 00:57:02 -0700

    Updates

---
+++ precache-manifest.371e4b64eca24655e597cf64b1575346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "afc6b0896d397f8a0fb0",
+    "url": "/clicky-game/static/js/main.afc6b089.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "afc6b0896d397f8a0fb0",
+    "url": "/clicky-game/static/css/main.9e560b7b.chunk.css"
+  },
+  {
+    "revision": "a51bb8322e23fb9d4af0f78b263a0364",
+    "url": "/clicky-game/index.html"
+  }
+];
